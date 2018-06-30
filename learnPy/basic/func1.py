d123d436030d8887f429d789ad9fd5ac0052df6c
vigaea <vigaea@gmail.com>
2018-06-30 12:47:12 +0800

    before

--- learnPy/basic/func1.py
+++
@@ -1,5 +0,0 @@
-n1 = 225
-n2 = 1000
-print('%s, %s',(hex(n1),hex(n2)))
-
-print(n2**2)
--- learnPy/highfeature/do_listcompr.py
+++
@@ -1,8 +0,0 @@
-
-L1 = ['Hello', 'World', 18, 'Apple', None]
-L2 = [s.lower() for s in L1 if isinstance(s, str)]
-print(L2)
-if L2 == ['hello', 'world', 'apple']:
-    print('测试通过!')
-else:
-    print('测试失败!')
